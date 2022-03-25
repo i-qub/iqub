f21d66b5396df52beec4568aad2d9c02c6db07d4
shankarutthandy <shankarnitteee@gmail.com>
2022-03-25 21:35:30 +0530

    init

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import styled from 'styled-components'
+const Nav=styled.div`
+width:77vw;
+height:12vh;
+display:flex;
+align-items:center;
+justify-content:space-between;
+margin:0px auto;
+font-family:sans-serif;
+& .nav-links{
+    width:60%;
+    display:flex;
+    justify-content:space-evenly;
+}
+& .nav-links span{
+    position:relative;
+    font-size:1.4rem;
+    font-weight:bolder;
+}
+& .nav-links span::after{
+    content: "";
+    background: linear-gradient(to top right, #9a4afc -2.43%, #ad3b96 -2.08%, #600271 1.42%, #b73360 100.72%, #ef8555 100.72%);
+    display: block;
+    position:absolute;
+    height:3px;
+    width:0;
+    left:0;
+    right:0;
+    bottom:-5px;
+    margin: auto;
+    transition: all 0.4s
+}
+& .nav-links span:hover::after,& .nav-links span.active::after{
+    width:100%;
+}
+& .nav-links span:hover,& .nav-links span.active{
+    background: linear-gradient(to top right, #9a4afc -2.43%, #ad3b96 -2.08%, #600271 1.42%, #b73360 100.72%, #ef8555 100.72%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+& .logo{
+    font-size:2rem;
+    font-weight:bolder;
+}
+& button{
+    padding:20px 16px;
+    font-size:1.1rem;
+    border:none;
+    border-radius:20px;
+    background:linear-gradient(to top right, #9a4afc -2.43%, #ad3b96 -2.08%, #600271 1.42%, #b73360 100.72%, #ef8555 100.72%);
+    color:white;
+}
+& button:hover{
+    background:linear-gradient(292.95deg,#9a4afc -17.22%,#ad3b96 -16.76%,#600271 -12.25%,#b73360 10.42%,#ef8555 107.4%);
+}
+`
+export default function NavBar() {
+  return (
+    <Nav>
+      <span className="logo">Logo</span>
+      <div className="nav-links">
+          <span className='pointer active'>Home</span>
+          <span className='pointer'>About</span>
+          <span className='pointer'>Carrer</span>
+      </div>
+      <button className='pointer'>Book a Demo</button>
+    </Nav>
+  )
+}
