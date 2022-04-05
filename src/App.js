4b9f83d2758ea59f2732760958cf5df8bcb1c7c9
shankarutthandy <shankarnitteee@gmail.com>
2022-04-05 12:47:38 +0530

    route fix

--- src/App.js
+++ src/App.js
@@ -35,9 +35,9 @@ function App() {
     <>
      <NavBar/>
      <Routes location={location} key={location.key}>
-     <Route path="/" exact element={<Home/>} />
-     <Route path="/about" exact element={<About/>} />
-     <Route path="/career" exact element={<Career/>} />
+     <Route path="/iqub/" exact element={<Home/>} />
+     <Route path="/iqub/about" exact element={<About/>} />
+     <Route path="/iqub/career" exact element={<Career/>} />
      </Routes>
      <Footer/>
     </>
